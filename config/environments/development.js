10d9542db94844440bd8276d40f8bd2b1896b6e2
Sirbu Nicolae-Cezar <sirbunicolaecezar@gmail.com>
2017-02-25 11:39:02 +0200

    First nails commit

---
+++ config/environments/development.js
@@ -0,0 +1,5 @@
+module.exports = {
+  SECRET_TOKEN: 'Not that s3cret.',
+  DATABASE_URL: 'mongodb://localhost:27017',
+  PORT: 3001
+};
---
+++ config/environments/production.js
@@ -0,0 +1,5 @@
+module.exports = {
+  SECRET_TOKEN: process.env.SECRET_TOKEN,
+  DATABASE_URL: process.env.DATABASE_URL,
+  PORT: process.env.PORT
+};
